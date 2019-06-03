41ac3fcbc0f72f233febd69530be0751b485db4c
Luc Jaulmes <luc.jaulmes@bsc.es>
2019-06-03 18:16:35 +0200

    Make DUE injection able to record several samples
    Also print a lot more information, from all samples, and unify clocks
    using a 64 bit nanoseconds format.

--- libcatchroi/inject_err.c
+++ libcatchroi/inject_err.c
@@ -7,7 +7,7 @@
 #include <math.h>
 #include <err.h>
 #include <errno.h>
-#include <sys/time.h>
+#include <time.h>
 
 #include <unistd.h>
 #include <sys/mman.h>
@@ -18,9 +18,56 @@
 #include <linux/perf_event.h>
 #include <linux/hw_breakpoint.h>
 
+
+const int clockid = CLOCK_MONOTONIC_RAW;
+
+static inline uint64_t getns()
+{
+	struct timespec get_time;
+	clock_gettime(clockid, &get_time);
+
+	return get_time.tv_sec * 1000000000 + get_time.tv_nsec;
+}
+
+
 #ifdef __x86_64__
 #include "xed/xed-interface.h"
+
+const char * const reg_names[] = {
+	"AX", "BX", "CX", "DX",
+	"SI", "DI", "BP", "SP",
+	"IP", "FLAGS", "CS", "SS",
+	"DS", "ES", "FS", "GS", // <- those 4 can not be sampled for some reason?
+	"R8", "R9", "R10", "R11",
+	"R12", "R13", "R14", "R15"
+};
+#define REGS 0xff0fff
+#define NREGS 20
 #endif
+
+#ifdef __powerpc64__
+const char * const reg_names[] = {
+	"R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7",
+	"R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15",
+	"R16", "R17", "R18", "R19", "R20", "R21", "R22", "R23",
+	"R24", "R25", "R26", "R27", "R28", "R29", "R30", "R31",
+	"NIP", "MSR", "ORIG_R3", "CTR",
+	"LINK", "XER", "CCR", "SOFTE",
+	"TRAP", "DAR", "DSISR", "SIER",
+	"MMCRA" // SIER and MMCRA can not be sampled
+};
+#define REGS 0x07ffffffffffL
+#define NREGS 43
+#endif
+
+#ifdef REGS
+_Static_assert(__builtin_popcountl(REGS) == NREGS, "register count does not match register mask");
+_Static_assert(NREGS < sizeof(reg_names), "too many registers in mask");
+#else
+#error "Undefined registers names, mask (and weight of mask) for this architecture: see arch/ARCH/include/uapi/asm/perf_regs.h"
+#endif
+
+
 
 #include "inject_err.h"
 
@@ -33,8 +80,8 @@ typedef struct _err
 {
 	union { int64_t mask; double mask_as_double; };
 	volatile int64_t *pos;
-	int64_t region, page;
-	double time;
+	int64_t region, page, tasks_finished;
+	uint64_t inject_time, start_time, real_inject_time, end_time;
 	int n_bits, perf_fd;
 	char type, undo, early;
 	_Atomic int inj;
@@ -45,7 +92,12 @@ typedef struct _err
 
 typedef struct _sample {
 	struct perf_event_header header;
-	intptr_t ip, data_src;
+	intptr_t ip;
+	uint32_t pid, tid;
+	uint64_t time, addr;
+	uint32_t cpu, _reserved;
+	uint64_t data_src;
+	uint64_t abi, regs[NREGS];
 } sample_t;
 
 
@@ -124,7 +176,7 @@ void inject_parse_env()
 		return;
 
 	int seed = 0;
-	err_t inject = {.pos = NULL, .type = NONE, .time = -1., .page = -1, .region = -1, .inj = 0};
+	err_t inject = {.pos = NULL, .type = NONE, .inject_time = 0, .page = -1, .region = -1, .inj = 0};
 
 	// Home-made option parsing, like getopts except don't call on it, since it interferes with calling it from main.
 	for (char *optarg = NULL, *optnext = argstr, *optstr = token(&optnext); optstr != NULL; optstr = token(&optnext), optarg = NULL)
@@ -142,7 +194,10 @@ void inject_parse_env()
 					n_tok_opts = 1;
 
 					if (long_options[lo].has_arg == required_argument)
+						// --long <val>
 						optarg = token(&optnext);
+
+						// TODO: --long=<val>
 
 					break;
 				}
@@ -198,7 +253,7 @@ void inject_parse_env()
 				inject.page   = strtoll(optarg, NULL, 0);
 				break;
 			case 'm':
-				inject.time   = atof(optarg) * (double)rand() / RAND_MAX;
+				inject.inject_time   = strtoull(optarg, NULL, 0) * (double)rand() / RAND_MAX;
 				break;
 			case 's':
 				seed          = atoi(optarg);
@@ -218,16 +273,11 @@ void inject_parse_env()
 
 	if (inject.type == NONE)
 		return;
-	else if (inject.time < 0 || (inject.page < 0 && inject.region < 0))
+	else if (inject.page < 0 && inject.region < 0)
 		err(1, "Wrong parameters");
 
-	if (seed == 0)
-	{
-		struct timeval t;
-		gettimeofday(&t, NULL);
-		seed = t.tv_sec + t.tv_usec;
-	}
-	srand(seed);
+	// seed == 0 to get a different seed every time
+	srand(seed ? seed : (int)getns());
 
 	if (inject.type == FLIP)
 		inject.mask = pick_bits(inject.n_bits);
@@ -257,12 +307,12 @@ double exponential(const double lambda, const double x)
 }
 
 static inline
-void sleep_ns(double ns)
+void sleep_ns(uint64_t ns)
 {
 	struct timespec next_sim_fault, remainder;
 
-	next_sim_fault.tv_sec  = (long long)floor(ns / 1e9);
-	next_sim_fault.tv_nsec = (long long)floor(ns - 1e9 * next_sim_fault.tv_sec);
+	next_sim_fault.tv_sec  = ns / 1000000000ULL;
+	next_sim_fault.tv_nsec = ns % 1000000000ULL;
 
 	if (nanosleep(&next_sim_fault, &remainder) != 0)
 		fprintf(stderr, "Nanosleep skipped %d.%09d of %d.%09d sleeping time\n",
@@ -291,9 +341,7 @@ void* inject_error(void* ignore)
 	err_t *error = ignore;
 
 	// default cancellability state + nanosleep is a cancellation point
-	sleep_ns(error->time);
-	int ntasks = roi_progress();
-	printf("inject completed_roi_tasks:%d", ntasks);
+	sleep_ns(error->inject_time);
 
 	int64_t *ptr = (int64_t*)error->pos;
 	if (ptr == NULL)
@@ -312,6 +360,8 @@ void* inject_error(void* ignore)
 		err(-1, "Unrecognised error type");
 
 	error->inj++;
+	error->real_inject_time = getns();
+	error->tasks_finished = roi_progress();
 
 	return NULL;
 }
@@ -327,8 +377,10 @@ void inject_start()
 		struct perf_event_attr pe = {
 			.type = PERF_TYPE_BREAKPOINT, .bp_type = HW_BREAKPOINT_RW, .bp_len = HW_BREAKPOINT_LEN_8, .bp_addr = (long long)error->pos,
 			.size = sizeof (struct perf_event_attr), .config = 0, .pinned = 1, .exclude_kernel = 1, .exclude_hv = 1,
-			.sample_period = 1, .sample_type = PERF_SAMPLE_IP | PERF_SAMPLE_DATA_SRC, .wakeup_events = 1, .precise_ip = 3
+			.sample_period = 1, .wakeup_events = 1, .precise_ip = 3, .sample_regs_intr = REGS, .use_clockid = 1, .clockid = clockid
 		};
+		pe.sample_type = PERF_SAMPLE_IP | PERF_SAMPLE_TIME | PERF_SAMPLE_TID | PERF_SAMPLE_CPU | PERF_SAMPLE_ADDR | \
+						 PERF_SAMPLE_REGS_INTR | PERF_SAMPLE_DATA_SRC;
 
 		error->perf_fd = syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
 		if (error->perf_fd < 0 || MAP_FAILED == (
@@ -339,12 +391,14 @@ void inject_start()
 		ioctl(error->perf_fd, PERF_EVENT_IOC_DISABLE, 0);
 	}
 
-	printf("inject_flip:%d inject_mask:%#016lx inject_dbl:%g inject_addr:%p inject_back:%d inject_time:%.09fs\n",
-			error->type, error->mask, error->mask_as_double, (void*)error->pos, error->undo, error->time / 1e9);
+	printf("inject_type:%d inject_mask:%#016lx inject_dbl:%g inject_addr:%p inject_back:%d inject_time:%lu\n",
+			error->type, error->mask, error->mask_as_double, (void*)error->pos, error->undo, error->inject_time);
 	fflush(stdout);
 
 	if (!pthread_create(&error->injector_thread, NULL, &inject_error, (void*)error) == 0)
 		err(errno, "Failed to create the injector thread");
+
+	error->start_time = getns();
 }
 
 
@@ -353,9 +407,15 @@ void inject_stop()
 	if (error == NULL)
 		return;
 
+	error->end_time = getns();
+
 	/* Print whether we flipped anything or whether the inject region stopped earlier */
 	char buf[1024];
-	snprintf(buf, sizeof(buf), "inject_done:%d", error->inj);
+	size_t len = 0;
+	#define safe_sprintf(...) do { len += snprintf(buf + len, sizeof(buf) - len, __VA_ARGS__); } while(0)
+
+	safe_sprintf("inject_done:%d end_time:%lu inject_finished_tasks:%ld inject_real_time:%lu",
+				error->inj, error->end_time - error->start_time, error->tasks_finished, error->real_inject_time - error->start_time);
 
 	pthread_join(error->injector_thread, NULL);
 
@@ -373,60 +433,93 @@ void inject_stop()
 
 	if (error->type == DUE)
 	{
-		if (error->event_map->data_tail < error->event_map->data_head)
-		{
-			sample_t *sample = (sample_t*)((intptr_t)error->event_map + error->event_map->data_offset + error->event_map->data_tail);
-			if (sample->header.type != PERF_RECORD_SAMPLE || sample->header.size != sizeof(*sample))
-				errx(1, "Unexpected sample metadata");
+		safe_sprintf(" inject_samples:%lu", (size_t)(error->event_map->data_head - error->event_map->data_tail) / sizeof(sample_t));
 
-			int mem_op = (sample->data_src >> PERF_MEM_OP_SHIFT) & (PERF_MEM_OP_LOAD | PERF_MEM_OP_STORE);
-
-			size_t len = strnlen(buf, sizeof(buf));
-			snprintf(buf + len, sizeof(buf) - len, " inject_samples:%llu sample_header:%x sample_precise:%d perf_raw_mem_op:%d",
-					(error->event_map->data_head - error->event_map->data_tail) / sizeof(sample_t),
-					sample->header.misc, (sample->header.misc & PERF_RECORD_MISC_EXACT_IP) != 0, mem_op);
 #ifdef __x86_64__
-			// Use XED to decode instructions, in particular find out if it was reading or writing
-			xed_tables_init();
+		// Use XED to decode instructions, in particular find out if it was reading or writing
+		xed_tables_init();
 
-			xed_decoded_inst_t xedd = {0};
-			xed_decoded_inst_zero(&xedd);
-			xed_decoded_inst_set_mode(&xedd, XED_MACHINE_MODE_LONG_64, XED_ADDRESS_WIDTH_64b);
+		xed_decoded_inst_t xedd = {0};
+		xed_decoded_inst_zero(&xedd);
+		xed_decoded_inst_set_mode(&xedd, XED_MACHINE_MODE_LONG_64, XED_ADDRESS_WIDTH_64b);
+#endif
 
-			if (xed_decode(&xedd, XED_STATIC_CAST(const xed_uint8_t*, sample->ip), 15) != XED_ERROR_NONE)
-				printf("xed_decode failed\n");
-
-			size_t memops = xed_decoded_inst_number_of_memory_operands(&xedd);
-			len = strnlen(buf, sizeof(buf));
-			snprintf(buf + len, sizeof(buf) - len, " sample_memops:%lu", memops);
-
-			for (unsigned m = 0; m < memops; m++)
+		const intptr_t evt_start = (intptr_t)error->event_map + error->event_map->data_offset + error->event_map->data_tail;
+		const intptr_t evt_end   = (intptr_t)error->event_map + error->event_map->data_offset + error->event_map->data_head;
+		sample_t *sample = NULL;
+		for (intptr_t evtptr = evt_start; evtptr != evt_end; evtptr += sample->header.size)
+		{
+			sample_t *sample = (sample_t*)evtptr;
+			if (sample->header.type != PERF_RECORD_SAMPLE || sample->header.size != sizeof(*sample))
 			{
-				len = strnlen(buf, sizeof(buf));
-				snprintf(buf + len, sizeof(buf) - len, " memop%u_read:%d memop%u_write:%d memop%u_writeonly:%d",
-					m, xed_decoded_inst_mem_read(&xedd, 0), m, xed_decoded_inst_mem_written(&xedd, 0),
-					m, xed_decoded_inst_mem_written_only(&xedd, 0));
+				warnx("Unexpected sample metadata: type=%u size=%u", sample->header.type, sample->header.size);
+				continue;
 			}
-#elif defined(__powerpc64__)
-			uint32_t *instr = (uint32_t*)sample->ip;
 
-			for (int i = -1; i < 2; i++)
+			// print sample data
+			len = 0;
+			printf("%s\n", buf);
+			safe_sprintf("sample_precise:%d sample_pc:%#016lx sample_time:%lu",
+					(sample->header.misc & PERF_RECORD_MISC_EXACT_IP) != 0, sample->ip, sample->time - error->start_time);
+
+			// only print sample meta-data if it does not fit with the expected vluaes
+			if ((sample->header.misc & PERF_RECORD_MISC_CPUMODE_MASK) != PERF_RECORD_MISC_USER) {
+				safe_sprintf(" sample_cpu_mode:%d", sample->header.misc & PERF_RECORD_MISC_CPUMODE_MASK);
+			}
+
+			if (sample->addr != (uintptr_t)error->pos) {
+				safe_sprintf(" sample_address:%#016lx", sample->addr);
+			}
+
+			if (sample->data_src) { // would be helpful but is usually just 0
+				int mem_op  = (sample->data_src >> PERF_MEM_OP_SHIFT);
+				int mem_lvl = (sample->data_src >> PERF_MEM_LVL_SHIFT);
+				safe_sprintf(" sample_datasrc_memop:%x sample_datasrc_memlvl:%x", mem_op, mem_lvl);
+			}
+
+			if (sample->abi != PERF_SAMPLE_REGS_ABI_64) {
+				safe_sprintf(" sample_regs_abi:%s", sample->abi ? "32b" : "none");
+			}
+
+			// print all the registers
+			for (size_t reg = 0; reg < sizeof(reg_names); reg++)
+				if (REGS & (1 << reg))
+					printf(" sample_%s:%016lx", reg_names[reg], sample->regs[reg]);
+
+			// finally decode
+#ifdef __x86_64__
+			if (xed_decode(&xedd, XED_STATIC_CAST(const xed_uint8_t*, sample->ip), 15) != XED_ERROR_NONE)
+				safe_sprintf("xed_decode failed");
+
+			else
 			{
-				uint32_t primary_opcode = (instr[i] >> 26) & 0x3fU;
-				uint32_t st = (primary_opcode & 0x24U) == 0x24U;
-				uint32_t ld = (primary_opcode & 0x24U) == 0x20U;
+				size_t memops = xed_decoded_inst_number_of_memory_operands(&xedd);
+				safe_sprintf(" sample_memops:%lu", memops);
 
-				len = strnlen(buf, sizeof(buf));
-				snprintf(buf + len, sizeof(buf) - len,
-						"\n\tsample_offset:%2d sample_addr:%p sample_instr:%x primary_opcode:%d memop_read:%u memop_write:%u",
-						i, (void*)(instr + i), instr[i], primary_opcode, ld, st);
+				for (unsigned m = 0; m < memops; m++)
+				{
+					len = strnlen(buf, sizeof(buf));
+					safe_sprintf(" memop%u_read:%d memop%u_write:%d memop%u_writeonly:%d",
+						m, xed_decoded_inst_mem_read(&xedd, 0),
+						m, xed_decoded_inst_mem_written(&xedd, 0),
+						m, xed_decoded_inst_mem_written_only(&xedd, 0));
+				}
 			}
 #else
+# ifdef __powerpc64__
+			uint32_t *instr = (uint32_t*)sample->ip;
+
+			uint32_t primary_opcode = (*instr >> 26) & 0x3fU;
+			uint32_t st = (primary_opcode & 0x24U) == 0x24U;
+			uint32_t ld = (primary_opcode & 0x24U) == 0x20U;
+
+			safe_sprintf("sample_addr:%p sample_instr:%x primary_opcode:%d memop_read:%u memop_write:%u",
+						(void*)instr, *instr, primary_opcode, ld, st);
+# else
 #  error "Architecture not implemented for decoding instructions"
+# endif
 #endif
 		}
-		else
-			strncat(buf, " inject_samples:0", sizeof(buf) - strnlen(buf, sizeof(buf)));
 
 		munmap(error->event_map, error->event_map->data_offset + error->event_map->data_size);
 		close(error->perf_fd);
